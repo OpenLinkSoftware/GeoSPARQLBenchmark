fb7b7aed0d7a11a5d85729e73dfe1dc87a3dab78
Timo <situx@users.noreply.github.com>
2021-02-04 19:59:01 +0100

    Update validatexml.py

--- validatexml.py
+++ validatexml.py
@@ -4,8 +4,20 @@ import glob
 import os, os.path, sys
 import pdb
 import fileinput
+from rdflib.plugins.sparql import prepareQuery
 
 path = os.getcwd()
+
+f = open("validationresult.txt", "w")
+for filenames in glob.glob(os.path.join("src/main/resources/gsb_queries/", '*.rq')):
+    try:
+        with open(filenames, 'r') as file:
+            query = file.read().replace('\n', '')
+        prepareQuery(query)
+        #print ("%s is well-formed" % filenames)
+    except Exception as e:
+        print ("%s is NOT well-formed!\nError: %s" % (filenames,e))
+        f.write(str(filenames)+" is not well-formed:\nError: "+str(e)+"\n")
 
 for filenames in glob.glob(os.path.join("src/main/resources/gsb_answers/", '*.srx')):
     try:
@@ -13,5 +25,7 @@ for filenames in glob.glob(os.path.join("src/main/resources/gsb_answers/", '*.sr
         parser.setContentHandler(ContentHandler())
         parser.parse(filenames)
         #print ("%s is well-formed" % filenames)
-    except Exception:
-        print ("%s is NOT well-formed!" % (filenames))
+    except Exception as e:
+        print ("%s is NOT well-formed!\nError: %s" % (filenames,e))
+        f.write(str(filenames)+" is not well-formed:\nError: "+str(e)+"\n")		
+f.close()
