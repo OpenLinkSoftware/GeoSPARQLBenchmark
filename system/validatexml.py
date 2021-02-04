5e0205ae8b81e867e1313b3c09a732da1ab563bf
Timo <situx@users.noreply.github.com>
2021-02-04 21:40:27 +0100

    Update validatexml.py

--- system/validatexml.py
+++ system/validatexml.py
@@ -4,24 +4,23 @@ import glob
 import os, os.path, sys
 import pdb
 import fileinput
-import rdflib
 from rdflib.plugins.sparql import prepareQuery
 
 path = os.getcwd()
-print("SPARQL and XML Validation Script")
-print("...Validating SPARQL queries...")
+
 f = open("validationresult.txt", "w")
-for filenames in glob.glob(os.path.join("../src/main/resources/gsb_queries/", '*.rq')):
+for filenames in glob.glob(os.path.join("src/main/resources/gsb_queries/", '*.rq')):
     try:
         with open(filenames, 'r') as file:
-            query = file.read().replace('\n', '')
+            query = file.read()
+        #print(query)
         prepareQuery(query)
         #print ("%s is well-formed" % filenames)
     except Exception as e:
         print ("%s is NOT well-formed!\nError: %s" % (filenames,e))
         f.write(str(filenames)+" is not well-formed:\nError: "+str(e)+"\n")
-print("...Validating Query answers...")
-for filenames in glob.glob(os.path.join("../src/main/resources/gsb_answers/", '*.srx')):
+
+for filenames in glob.glob(os.path.join("src/main/resources/gsb_answers/", '*.srx')):
     try:
         parser = make_parser()
         parser.setContentHandler(ContentHandler())
@@ -31,4 +30,3 @@ for filenames in glob.glob(os.path.join("../src/main/resources/gsb_answers/", '*
         print ("%s is NOT well-formed!\nError: %s" % (filenames,e))
         f.write(str(filenames)+" is not well-formed:\nError: "+str(e)+"\n")		
 f.close()
-print("Finished")
