eb2c8b06d73f16a09abb204e0ff48558b9556a27
Timo <situx@users.noreply.github.com>
2021-06-24 15:30:27 +0200

    Update benchmark_geosparql10.py

--- benchmark_geosparql10.py
+++ benchmark_geosparql10.py
@@ -11,8 +11,6 @@ resultMapReq={}
 
 queryResultMap={}
 
-percentagePerReq=0.0333
-
 inference=["query-r25-1.rq","query-r25-2.rq","query-r25-3.rq","query-r26-1.rq","query-r26-2.rq","query-r27.rq"]
 
 benchmarkname="GeoSPARQL 1.0 Compliance Benchmark"
@@ -23,7 +21,217 @@ extensionMap={"CORE":["query-r01","query-r02","query-r03"],
 			  "GTOP":["query-r21","query-r22","query-r23","query-r24"],
 			  "RDFSE":["query-r25","query-r26","query-r27"],
 			  "QRW":["query-r28","query-r29","query-r30"]}
-			  
+
+reqWeights={
+    "query-r01.rq":1.0/30.0,
+	"query-r02.rq":1.0/30.0,
+	"query-r03.rq":1.0/30.0,
+	"query-r04-1.rq":1.0/30.0 * 1.0/8.0,
+	"query-r04-2.rq":1.0/30.0 * 1.0/8.0,
+	"query-r04-3.rq":1.0/30.0 * 1.0/8.0,
+	"query-r04-4.rq":1.0/30.0 * 1.0/8.0,
+    "query-r04-5.rq":1.0/30.0 * 1.0/8.0,
+	"query-r04-6.rq":1.0/30.0 * 1.0/8.0,
+	"query-r04-7.rq":1.0/30.0 * 1.0/8.0,
+	"query-r04-8.rq":1.0/30.0 * 1.0/8.0,
+	"query-r05-1.rq":1.0/30.0 * 1.0/8.0,
+	"query-r05-2.rq":1.0/30.0 * 1.0/8.0,
+	"query-r05-3.rq":1.0/30.0 * 1.0/8.0,
+	"query-r05-4.rq":1.0/30.0 * 1.0/8.0,
+    "query-r05-5.rq":1.0/30.0 * 1.0/8.0,
+	"query-r05-6.rq":1.0/30.0 * 1.0/8.0,
+	"query-r05-7.rq":1.0/30.0 * 1.0/8.0,
+	"query-r05-8.rq":1.0/30.0 * 1.0/8.0,
+	"query-r06-1.rq":1.0/30.0 * 1.0/8.0,
+	"query-r06-2.rq":1.0/30.0 * 1.0/8.0,
+	"query-r06-3.rq":1.0/30.0 * 1.0/8.0,
+	"query-r06-4.rq":1.0/30.0 * 1.0/8.0,
+    "query-r06-5.rq":1.0/30.0 * 1.0/8.0,
+	"query-r06-6.rq":1.0/30.0 * 1.0/8.0,
+	"query-r06-7.rq":1.0/30.0 * 1.0/8.0,
+	"query-r06-8.rq":1.0/30.0 * 1.0/8.0,
+	"query-r07.rq":1.0/30.0,
+	"query-r08-1.rq":1.0/30.0 * 1.0/2.0,
+	"query-r08-2.rq":1.0/30.0 * 1.0/2.0,
+	"query-r09-1.rq":1.0/30.0 * 1.0/6.0,
+    "query-r09-2.rq":1.0/30.0 * 1.0/6.0,
+	"query-r09-3.rq":1.0/30.0 * 1.0/6.0,
+	"query-r09-4.rq":1.0/30.0 * 1.0/6.0,
+	"query-r09-5.rq":1.0/30.0 * 1.0/6.0,
+	"query-r09-6.rq":1.0/30.0 * 1.0/6.0,
+	"query-r10.rq":1.0/30.0,
+	"query-r11.rq":1.0/30.0,
+	"query-r12.rq":1.0/30.0,
+	"query-r13-1.rq":1.0/30.0 * 1.0/2.0,
+	"query-r13-2.rq":1.0/30.0 * 1.0/2.0,
+	"query-r14.rq":1.0/30.0,
+	"query-r15.rq":1.0/30.0,
+	"query-r16-1.rq":1.0/30.0 * 1.0/2.0,
+	"query-r16-2.rq":1.0/30.0 * 1.0/2.0,
+	"query-r18.rq":1.0/30.0,
+	"query-r19-1-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-1-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-1-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-1-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-2-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r19-2-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r19-3-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r19-3-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r19-4-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-4-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-4-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-4-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-5-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-5-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-5-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-5-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-6-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-6-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-6-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-6-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-7-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-7-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r19-7-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-7-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r19-8-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r19-8-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r19-9-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r19-9-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r20-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r20-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/2.0,
+	"query-r21-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r21-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r21-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r21-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-1-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-1-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-1-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-1-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-2-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-2-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-2-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-2-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-3-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-3-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-3-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-3-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-4-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-4-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-4-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-4-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-5-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-5-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-5-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-5-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-6-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-6-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-6-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-6-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-7-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-7-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-7-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-7-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-8-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-8-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r22-8-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r22-8-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-1-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-1-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-1-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-1-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-2-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-2-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-2-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-2-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-3-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-3-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-3-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-3-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-4-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-4-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-4-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-4-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-5-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-5-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-5-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-5-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-6-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-6-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-6-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-6-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-7-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-7-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-7-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-7-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-8-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-8-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r23-8-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r23-8-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-1-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-1-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-1-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-1-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-2-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-2-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-2-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-2-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-3-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-3-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-3-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-3-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-4-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-4-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-4-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-4-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-5-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-5-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-5-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-5-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-6-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-6-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-6-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-6-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-7-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-7-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-7-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-7-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-8-1.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-8-2.rq":1.0/30.0 * 1.0/9.0 * 1.0/3.0,
+	"query-r24-8-3.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r24-8-4.rq":1.0/30.0 * 1.0/9.0 * 1.0/6.0,
+	"query-r25-1.rq":1.0/30.0 * 1.0/3.0,
+	"query-r25-2.rq":1.0/30.0 * 1.0/3.0,
+	"query-r25-3.rq":1.0/30.0 * 1.0/3.0,
+	"query-r26-1.rq":1.0/30.0 * 1.0/2.0,
+	"query-r26-2.rq":1.0/30.0 * 1.0/2.0,
+	"query-r27.rq":1.0/30.0,
+	"query-r28-1.rq":1.0/30.0 * 1.0/8.0,
+	"query-r28-2.rq":1.0/30.0 * 1.0/8.0,
+	"query-r28-3.rq":1.0/30.0 * 1.0/8.0,
+	"query-r28-4.rq":1.0/30.0 * 1.0/8.0,
+	"query-r28-5.rq":1.0/30.0 * 1.0/8.0,
+	"query-r28-6.rq":1.0/30.0 * 1.0/8.0,
+	"query-r28-7.rq":1.0/30.0 * 1.0/8.0,
+	"query-r28-8.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-1.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-2.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-3.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-4.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-5.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-6.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-7.rq":1.0/30.0 * 1.0/8.0,
+	"query-r29-8.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-1.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-2.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-3.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-4.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-5.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-6.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-7.rq":1.0/30.0 * 1.0/8.0,
+	"query-r30-8.rq":1.0/30.0 * 1.0/8.0
+}
+
+
 reqToURI={
 "query-r01":"http://www.opengis.net/spec/geosparql/1.0/req/core/sparql-protocol",
 "query-r02":"http://www.opengis.net/spec/geosparql/1.0/req/core/spatial-object-class",
@@ -139,7 +347,7 @@ def escape_cdata(s):
 def calculateComplianceScore(resultMap):
 	correctScore=0
 	amountOfQueries=len(resultMap)
-	compliancescore=0
+	compliancescore=0.01
 	extensionScores={}
 	if amountOfQueries==0:
 		return 0
@@ -148,8 +356,9 @@ def calculateComplianceScore(resultMap):
 		if resultMap[res]:
 			correctScore+=1
 	for res in resultMapReq:
-		percperThisReq=percentagePerReq/len(resultMapReq[res])
+		 #percentagePerReq/len(resultMapReq[res])
 		for assessment in resultMapReq[res]:
+			percperThisReq=reqWeights[assessment]
 			if resultMapReq[res][assessment]:
 				compliancescore+=percperThisReq
 		print("ComplianceScore: "+str(round(compliancescore*100,2))+"%")
@@ -160,17 +369,19 @@ def calculateComplianceScore(resultMap):
 	f.write("ComplianceScore: "+str(round(compliancescore*100,2))+"%\n")
 	for ext in extensionMap:
 		reqlist=extensionMap[ext]
-		extmaxscore=len(extensionMap[ext])*percentagePerReq
+		extmaxscore=0 #len(extensionMap[ext])*percentagePerReq
 		curextscore=0
 		for req in reqlist:
-			print(resultMapReq)
+			#print(resultMapReq)
 			if req in resultMapReq:
-				percperThisReq=percentagePerReq/len(resultMapReq[req])
+				#percperThisReq=reqWeights[req] #percentagePerReq/len(resultMapReq[req])
 				for assessment in resultMapReq[req]:
+					percperThisReq=reqWeights[assessment]
 					if resultMapReq[req][assessment]:
 						curextscore+=percperThisReq
-			else:
-				curextscore+=percentagePerReq
+					extmaxscore+=percperThisReq
+			#else:
+			#	curextscore+=percentagePerReq
 		f.write("Extension "+str(ext)+": "+str(round((curextscore/extmaxscore)*100,2))+"%\n")
 	f.close()
 
@@ -181,16 +392,17 @@ def benchmarkResultsToRDF(resultMap):
 	ttlstring+="geo:hasEvaluationQuery rdf:type owl:ObjectProperty .\n"
 	for ext in extensionMap:
 		reqlist=extensionMap[ext]
-		extmaxscore=len(extensionMap[ext])*percentagePerReq
+		extmaxscore=0
 		curextscore=0
 		for req in reqlist:
 			ttlstring+="<"+reqToURI[req]+"> rdf:type geo:Requirement .\n"
 			ttlstring+="<"+reqToURI[req]+"_eval> rdf:type geo:RequirementEvaluation .\n"
-			print(resultMapReq)
+			#print(resultMapReq)
 			if req in resultMapReq:
-				percperThisReq=percentagePerReq/len(resultMapReq[req])
+				#percperThisReq=reqWeights[req]#percentagePerReq/len(resultMapReq[req])
 				asscounter=1
 				for assessment in resultMapReq[req]:
+					percperThisReq=reqWeights[assessment]
 					ttlstring+="<"+reqToURI[req]+"_eval> geo:hasEvaluationQuery <"+reqToURI[req]+"/query_"+str(asscounter)+"> .\n"
 					ttlstring+="<"+reqToURI[req]+"/query_"+str(asscounter)+"> rdf:type geo:RequirementQuery .\n"
 					ttlstring+="<"+reqToURI[req]+"/query_"+str(asscounter)+"> rdfs:label \""+str(reqToURI[req])+" query "+str(asscounter)+"\"@en .\n"
@@ -199,8 +411,9 @@ def benchmarkResultsToRDF(resultMap):
 					asscounter+=1
 					if resultMapReq[req][assessment]:
 						curextscore+=percperThisReq
-			else:
-				curextscore+=percentagePerReq
+					extmaxscore+=percperThisReq
+			#else:
+			#	curextscore+=percentagePerReq
 		ttlstring+="<"+reqToURI[req]+"_eval> geo:evaluation \""+str(curextscore)+"\"^^xsd:double .\n"
 	f = open("benchmarkresult.ttl", "w")
 	f.write(ttlstring)
@@ -211,7 +424,7 @@ def getResultFilesList(testid):
 	results={}
 	for file in os.listdir(resultFolder):
 		if file.startswith(testid.replace(".rq","")):
-			print("File: "+str(file))
+			#print("File: "+str(file))
 			f = open(resultFolder+file.replace(".rq",".srx"), 'r')  
 			content = f.read()
 			results[file]=parseString(content).toprettyxml(indent="", newl="\n")
@@ -230,8 +443,8 @@ for name in os.listdir(queryFolder):
 	else:
 		queryEndpoint(curendpoint,content,resultFolder,name)
 	f.close()
-print(json.dumps(resultMap, indent=2))
-print(json.dumps(resultMapReq, indent=2))
+#print(json.dumps(resultMap, indent=2))
+#print(json.dumps(resultMapReq, indent=2))
 f = open("benchmarkresult.json", "w")
 f.write(json.dumps(resultMap, indent=2))
 f.close()
